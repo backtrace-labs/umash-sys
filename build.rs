dd79f4017351a1f97e71ad060adab66830156a20
Paul Khuong <pvk@pvk.ca>
2024-10-06 10:26:55 -0400

    Add feature for UMASH_LONG_INPUTS
    Size-optimised builds or libraries that only hash short inputs
    might want to disable the long input specialisation (it's also more
    complex and more likely to break).  Add a feature to turn that
    off, but enable the feature by default.

--- build.rs
+++ build.rs
@@ -3,10 +3,17 @@ fn main() {
     println!("cargo:rerun-if-changed=src/umash_long.inc");
     println!("cargo:rerun-if-changed=src/umash.h");
 
+    let long_inputs = if cfg!(feature = "long_input_routines") {
+        "1"
+    } else {
+        "0"
+    };
+
     cc::Build::new()
         .file("src/umash.c")
         .opt_level(2)
         .flag_if_supported("-mpclmul")
         .flag_if_supported("-march=armv8-a+crypto")
-        .compile("umash")
+        .define("UMASH_LONG_INPUTS", long_inputs)
+        .compile("umash");
 }
